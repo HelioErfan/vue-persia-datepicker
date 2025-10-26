6d992fff937b3ea0326e2285547bcdba02e97d96
Erfan Shirbakht <erfansh812004@gmail.com>
2025-10-26 14:33:37 +0330

    add: add montsh module

---
+++ src/components/jalaliConstants.js
@@ -0,0 +1,16 @@
+export default useJalaliMonths = () => {
+    return [
+        { name: 'فروردین', value: '01' },
+        { name: 'اردیبهشت', value: '02' },
+        { name: 'خرداد', value: '03' },
+        { name: 'تیر', value: '04' },
+        { name: 'مرداد', value: '05' },
+        { name: 'شهریور', value: '06' },
+        { name: 'مهر', value: '07' },
+        { name: 'آبان', value: '08' },
+        { name: 'آذر', value: '09' },
+        { name: 'دی', value: '10' },
+        { name: 'بهمن', value: '11' },
+        { name: 'اسفند', value: '12' }
+    ]
+}
