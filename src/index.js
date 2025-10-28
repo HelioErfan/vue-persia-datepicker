d50565914363b6381f124473d53da5ed2b0ca298
Erfan Shirbakht <erfansh812004@gmail.com>
2025-10-28 12:14:42 +0330

    chore: change the name of package

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import DatePicker from "./components/DatePicker.vue";
 
 export default {
     install(app) {
-        app.component('PersianDatePicker', DatePicker)
+        app.component('VuePersiaDatePicker', DatePicker)
     }
 }
 
