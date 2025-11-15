dc85f5bcdf4cc5ba677e7bfbe4042b7928ed9701
Erfan Shirbakht <erfansh812004@gmail.com>
2025-11-15 16:02:30 +0330

    chore: prepare for private publish

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,5 @@
-<script setup>
-import DatePicker from './components/DatePicker.vue'
-import { ref } from 'vue'
-
-const selectedDate = ref(null)
-const selectedRange = ref({ start: null, end: null })
-</script>
+<script setup></script>
 
 <template>
-    <div id="app">
-        <div style="display: flex; align-items: center; justify-content: center; flex-direction: column;">
-            <p>سینگل</p>
-            <DatePicker v-model:date="selectedDate" mode="single" />
-            <p>رنج</p>
-            <DatePicker v-model:range="selectedRange" mode="range" />
-            <p>selectedDate: {{ selectedDate }}</p>
-            <p>selectedRange: {{ selectedRange }}</p>
-            <p>selectedRange start: {{ selectedRange.start }}</p>
-            <p>selectedRange end: {{ selectedRange.end }}</p>
-        </div>
-    </div>
+    <div id="app"></div>
 </template>
